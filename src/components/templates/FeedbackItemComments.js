3c62ced9154eaf8357bd0b17883bfe26741d53d9
ebi <ebi@gmail.com>
2024-07-20 00:54:10 +0330

    FeedbackItemComments started

---
+++ src/components/templates/FeedbackItemComments.js
@@ -0,0 +1,17 @@
+import TextareaInput from "../module/TextareaInput"
+
+
+
+
+function FeedbackItemComments() {
+  return (
+    <div>
+      <h3>all comments</h3>
+      <form>
+        <TextareaInput/>
+      </form>
+    </div>
+  )
+}
+
+export default FeedbackItemComments
