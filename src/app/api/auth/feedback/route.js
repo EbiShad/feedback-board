635e31d9d6640ddfdee8c617c2bf8adf256cc311
ebi <ebi@gmail.com>
2024-07-21 13:38:05 +0330

    Modal component is changed

--- src/app/api/auth/feedback/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-
-
-export async function POST(req){
-    
-    return NextResponse.json({ message: "Enter email or password please" ,status: 422 })
-}
