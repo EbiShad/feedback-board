d3a6a8987cfb676141c145f2e9f69485f78fb63c
ebi <ebi@gmail.com>
2024-07-25 01:51:21 +0330

    handleAttachFileInputChange added to project by using supabase db

--- src/components/templates/FeedbackFormModal.js
+++ src/components/templates/FeedbackFormModal.js
@@ -7,11 +7,12 @@ import Button from "../module/Button";
 import { useMutation } from "@tanstack/react-query";
 import { createPostFn } from "@/servises/postService";
 import toast from "react-hot-toast";
-import axios from "axios";
+import supabase from "@/config/SupabaseClient";
 
 function FeedbackFormModal({ onClose }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [imgUpload, setImgUpload] = useState([]);
 
   const { data, isPending, mutateAsync } = useMutation({
     mutationFn: createPostFn,
@@ -28,23 +29,22 @@ function FeedbackFormModal({ onClose }) {
     onError: (err) => {
       toast.error(err.message);
     },
-  })
+  });
 
   const handleCreatePostButton = async (e) => {
     e.preventDefault();
     const data = await mutateAsync({ title, description });
-  }
+  };
 
-  const handleAttachFileInputChange = async(e) => {
-   const files = [...e.target.files]
-   const data = new FormData()
-   for (const file in files){
-      data.append("file",file)
-   }
+  const handleAttachFileInputChange = async (e) => {
+    const file = e.target.files[0];
 
-  const response = await axios.post("/api/upload",data).then(res => res.data)
-  console.log(response)
-  }
+    const links = [];
+    const { data } = await supabase.storage.from("pictures").upload(`${file.name}`, file);
+    const imageUrl = `https://dddfealtiyhdqcdfvznh.supabase.co/storage/v1/object/public/pictures/${file.name}`;
+    links.push(imageUrl)
+    setImgUpload((prev) => [...prev,...links])
+  };
 
   return (
     <form className="space-y-4">
@@ -64,12 +64,31 @@ function FeedbackFormModal({ onClose }) {
         onChange={(e) => setDescription(e.target.value)}
         placeholder="please include any details"
       />
+      
+      {imgUpload?.length > 0 && (
+        <div>
+        <label>Files</label>
+          <div className="flex gap-2 p-1 border h-20 rounded-md mt-1">
+            {imgUpload.map((link,index) => (
+              <a href={link} target="_blank" key={index} className="w-1/5">
+                <img src={link} alt={link} className="h-full w-full rounded-md object-cover"/>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+
       <div className="flex items-center gap-4 justify-end">
         <label>
           <span className="bg-gray-300 hover:bg-gray-400 px-2 py-2 rounded-lg cursor-pointer flex justify-center text-black items-center transition-colors duration-300">
             Atach files
           </span>
-          <input type="file" className="hidden" onChange={handleAttachFileInputChange}/>
+          <input
+            multiple
+            type="file"
+            className="hidden"
+            onChange={handleAttachFileInputChange}
+          />
         </label>
         <Button onClick={handleCreatePostButton}>Create post</Button>
       </div>
