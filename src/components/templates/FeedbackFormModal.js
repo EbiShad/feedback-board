f89291b0501bf6afd4e0b2226087ad5ce97fe974
ebi <ebi@gmail.com>
2024-07-25 13:46:29 +0330

    handleremoveFileButton added to project

--- src/components/templates/FeedbackFormModal.js
+++ src/components/templates/FeedbackFormModal.js
@@ -8,6 +8,7 @@ import { useMutation } from "@tanstack/react-query";
 import { createPostFn } from "@/servises/postService";
 import toast from "react-hot-toast";
 import supabase from "@/config/SupabaseClient";
+import { IoTrashOutline } from "react-icons/io5";
 
 function FeedbackFormModal({ onClose }) {
   const [title, setTitle] = useState("");
@@ -40,11 +41,18 @@ function FeedbackFormModal({ onClose }) {
     const file = e.target.files[0];
 
     const links = [];
-    const { data } = await supabase.storage.from("pictures").upload(`${file.name}`, file);
-    const imageUrl = `https://dddfealtiyhdqcdfvznh.supabase.co/storage/v1/object/public/pictures/${file.name}`;
+    const { data } = await supabase.storage.from("pictures").upload(`${file.name}`, file)
+    const imageUrl = `https://dddfealtiyhdqcdfvznh.supabase.co/storage/v1/object/public/pictures/${file.name}`
     links.push(imageUrl)
     setImgUpload((prev) => [...prev,...links])
-  };
+  }
+
+  const handleremoveFileButton = (e,link) => {
+    e.preventDefault();
+    setImgUpload(currentUpload => {
+      return currentUpload.filter(val => val !== link)
+    })
+  }
 
   return (
     <form className="space-y-4">
@@ -67,10 +75,13 @@ function FeedbackFormModal({ onClose }) {
       
       {imgUpload?.length > 0 && (
         <div>
-        <label>Files</label>
-          <div className="flex gap-2 p-1 border h-20 rounded-md mt-1">
+          <label>Files</label>
+          <div className="flex gap-2 p-1 border h-20 rounded-md mt-1 ">
             {imgUpload.map((link,index) => (
-              <a href={link} target="_blank" key={index} className="w-1/5">
+              <a href={link} target="_blank" key={index} className="w-1/5 relative">
+                <button onClick={(e) => handleremoveFileButton(e,link)} className="absolute  -top-[6px] -right-[6px]"> 
+                 <IoTrashOutline className="bg-red-500 text-2xl text-gray-300 p-1 rounded-md"/> 
+                </button>
                 <img src={link} alt={link} className="h-full w-full rounded-md object-cover"/>
               </a>
             ))}
@@ -80,7 +91,8 @@ function FeedbackFormModal({ onClose }) {
 
       <div className="flex items-center gap-4 justify-end">
         <label>
-          <span className="bg-gray-300 hover:bg-gray-400 px-2 py-2 rounded-lg cursor-pointer flex justify-center text-black items-center transition-colors duration-300">
+          <span className="bg-gray-300 hover:bg-gray-400 px-2 py-2 rounded-lg cursor-pointer 
+          flex justify-center text-black items-center transition-colors duration-300">
             Atach files
           </span>
           <input
