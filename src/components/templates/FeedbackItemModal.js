135e56bf689790ddeaa47202a0d5740db3c53790
ebi <ebi@gmail.com>
2024-07-29 16:04:07 +0330

    voteButton added to FeedbackItemModal

--- src/components/templates/FeedbackItemModal.js
+++ src/components/templates/FeedbackItemModal.js
@@ -1,8 +1,29 @@
 import Button from "../module/Button";
 import { TbTriangleInvertedFilled } from "react-icons/tb";
 import FeedbackItemComments from "./FeedbackItemComments";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createVoteFn } from "@/servises/votesServices";
+import Loader from "../module/Loader";
 
-function FeedbackItemModal({ title, description, votesCount }) {
+function FeedbackItemModal({ title, description,votes,_id }) {
+  const votedVotes = votes?.filter( v => v.feedbackId === _id)
+
+  const queryClient = useQueryClient()
+
+  const { data, isPending:isVoting, mutateAsync } = useMutation({
+    mutationFn: createVoteFn,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["get-votes"],
+      })
+    }
+  })
+
+  const voteButton = async (e) => {
+     const data = await mutateAsync({feedbackId:_id})
+    
+ }
+
   return (
     <div>
       <div>
@@ -11,8 +32,10 @@ function FeedbackItemModal({ title, description, votesCount }) {
       </div>
 
       <div className="flex items-center mt-4 justify-end border-b-gray-500 border-b pb-2">
-        <Button>
-          <TbTriangleInvertedFilled className="w-3 h-3" /> Upvoted {votesCount}
+        <Button onClick={voteButton}>
+        {isVoting ? <Loader width={20} height={20} /> : 
+        <><TbTriangleInvertedFilled className="w-3 h-3" />Upvoted {votedVotes.length || 0}</>}
+         
         </Button>
       </div>
 
