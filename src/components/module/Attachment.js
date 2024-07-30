92769a760a34248ea28f52e5a84597b37f93ce97
ebi <ebi@gmail.com>
2024-07-31 00:51:29 +0330

    Attachment is changed

--- src/components/module/Attachment.js
+++ src/components/module/Attachment.js
@@ -1,15 +1,12 @@
 import { IoTrashOutline } from "react-icons/io5";
 
-function Attachment({ imgUpload ,showRemoveButton}) {
+function Attachment({ link ,showRemoveButton ,onDeletFile}) {
   return (
     <div>
-      <label>Files</label>
-      <div className="flex gap-2 p-1 border h-20 rounded-md mt-1 ">
-        {imgUpload.map((link, index) => (
-          <a href={link} target="_blank" key={index} className="w-1/5 relative">
+     <a href={link} target="_blank" className="w-1/5 relative">
           {showRemoveButton && 
             <button
-              onClick={(e) => handleremoveFileButton(e, link)}
+              onClick={onDeletFile}
               className="absolute -top-[6px] -right-[6px]"
             >
               <IoTrashOutline className="bg-red-500 text-2xl text-gray-300 p-1 rounded-md" />
@@ -22,8 +19,6 @@ function Attachment({ imgUpload ,showRemoveButton}) {
               className="h-full w-full rounded-md object-cover"
             />
           </a>
-        ))}
-      </div>
     </div>
   );
 }
