9fe8c0e90f9d63debb7265781dc463b65a16d8bd
ebi <ebi@gmail.com>
2024-07-31 01:32:00 +0330

    AttachFiles is added

--- src/components/templates/FeedbackFormModal.js
+++ src/components/templates/FeedbackFormModal.js
@@ -8,15 +8,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPostFn } from "@/servises/feedbackService";
 import toast from "react-hot-toast";
 import supabase from "@/config/SupabaseClient";
-import Loader from "../module/Loader";
 import Attachment from "../module/Attachment";
+import AttachFiles from "../module/AttachFiles";
 
 function FeedbackFormModal({ onClose }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [imgUpload, setImgUpload] = useState([]);
-  const [uploading,setUploading] = useState(false);
-
+  
   const queryClient = useQueryClient()
 
   const { data, isPending, mutateAsync } = useMutation({
@@ -45,16 +44,6 @@ function FeedbackFormModal({ onClose }) {
     const data = await mutateAsync({ title, description ,imgUpload});
   }
 
-  const handleAttachFileInputChange = async (e) => {
-    const file = e.target.files[0];
-    const links = [];
-    setUploading(true)
-    const { data } = await supabase.storage.from("pictures").upload(`${file.name}`, file)
-    const imageUrl = `https://dddfealtiyhdqcdfvznh.supabase.co/storage/v1/object/public/pictures/${file.name}`
-    links.push(imageUrl)
-    setImgUpload((prev) => [...prev,...links])
-    setUploading(false)
-  }
 
   const handleremoveFileButton = (e,link) => {
     e.preventDefault();
@@ -98,21 +87,8 @@ function FeedbackFormModal({ onClose }) {
       )}
 
       <div className="flex items-center gap-4 justify-end">
-        <label>
-          <span className={`
-          ${uploading ? "bg-gray-200 text-gray-400 cursor-not-allowed" : "bg-gray-300 hover:bg-gray-400 text-black cursor-pointer"} 
-            px-2 py-2 rounded-lg flex justify-center gap-2  items-center transition-colors duration-300`}>
-            {uploading && <Loader width={20} height={20} color="rgb(156 163 175)"/>}
-            {uploading ? "Uploading" : "Atach files"}
-          </span>
-          <input
-            type="file"
-            className="hidden"
-            onChange={handleAttachFileInputChange}
-            disabled={uploading}
-          />
-        </label>
-        <Button onClick={handleCreatePostButton}>Create post</Button>
+        <AttachFiles setImgUpload={setImgUpload}/>
+        <Button onClick={handleCreatePostButton}> Create post </Button>
       </div>
     </form>
   );
