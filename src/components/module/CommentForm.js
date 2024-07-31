96f2b88e961746832094919d7ef1a3bc5e5a8b18
ebi <ebi@gmail.com>
2024-07-31 16:22:03 +0330

    createCommentButton added to commentForm

--- src/components/module/CommentForm.js
+++ src/components/module/CommentForm.js
@@ -3,10 +3,32 @@ import Button from "./Button";
 import TextareaInput from "./TextareaInput";
 import AttachFiles from "./AttachFiles";
 import Attachment from "./Attachment";
+import { createCommentFn } from "@/servises/commentServices";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
-function CommentForm() {
+function CommentForm({feedbackId,onClose}) {
   const [commentText, setCommentText] = useState("");
   const [imgUpload, setImgUpload] = useState([]);
+
+  const { data, isPending:isCommenting, mutateAsync } = useMutation({
+    mutationFn: createCommentFn,
+    onSuccess: (data) => {
+      if (data.status === 201) {
+        toast.success(data.message);
+        setCommentText("");
+        setImgUpload([])
+        onClose()
+        return;
+      } else {
+        toast.error(data.message);
+      }
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    }
+    })
+
 
 
   const handleremoveFileButton = (e,link) => {
@@ -16,6 +38,10 @@ function CommentForm() {
     })
   }
 
+  const createCommentButton = async(e) => {
+    e.preventDefault();
+    const data = await mutateAsync({ commentText, feedbackId ,imgUpload})
+  }
 
 
   return (
@@ -32,7 +58,7 @@ function CommentForm() {
       {imgUpload?.length > 0 && (
         <div>
           <label>Files</label>
-          <div className="flex gap-2 p-1 border h-20 rounded-md mt-1 ">
+          <div className="flex gap-2 p-1 border h-20 rounded-md mt-1 mb-2">
             {imgUpload.map((link, index) => (
               <Attachment
                 key={index}
@@ -47,7 +73,7 @@ function CommentForm() {
 
       <div className="flex items-center gap-4 justify-end">
         <AttachFiles setImgUpload={setImgUpload} />
-        <Button disabled={commentText === ""}>Comment</Button>
+        <Button disabled={commentText === "" } onClick={createCommentButton}>Comment</Button>
       </div>
     </form>
   );
