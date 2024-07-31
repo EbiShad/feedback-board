91159b7edd3e30fb1ba07b211e85110ee219504b
ebi <ebi@gmail.com>
2024-07-31 22:39:46 +0330

    comments added

--- src/components/templates/FeedbackItemComments.js
+++ src/components/templates/FeedbackItemComments.js
@@ -1,25 +1,52 @@
-
+import { useState } from "react";
 import Avatar from "../module/Avatar";
 import CommentForm from "../module/CommentForm";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import TimeAgo from "timeago-react";
+import Attachment from "../module/Attachment";
 
-function FeedbackItemComments({feedbackId,onClose}) {
- 
+function FeedbackItemComments({ feedbackId = "" }) {
+  const [comments, setComments] = useState([]);
+
+  const { isPending: isLoadingComments } = useQuery({
+    queryFn: () => {
+      axios
+        .get(`/api/comment?feedbackId=${feedbackId}`)
+        .then((res) => setComments(res.data));
+      return;
+    },
+    queryKey: ["get-comments", feedbackId],
+  })
+
+
 
   return (
     <div>
-      <div className="mt-8 flex gap-4">
-        <Avatar />
-        <div>
-          <p className="text-justify text-xs">
-            mmonly used to the visual form of a document or a typeface
-            publishing and graphic design, Lorem ipsum is a placeholder text co
-            without relying on meaningful content. Lorem ipsum may be used as a
-            placehol
-          </p>
-          <div className="text-xs text-gray-400 pt-1">Anonymous . afew minutes ago</div>
-        </div>
-      </div>
-     <CommentForm feedbackId={feedbackId} onClose={onClose}/>
+      {comments?.length > 0 &&
+        comments.map((comment) => (
+          <div key={comment._id}>
+            <div className="mt-8 flex gap-4">
+              <Avatar />
+              <div>
+                <p className="text-justify text-xs">{comment.commentText}</p>
+                <div className="text-xs text-gray-400 pt-1">
+                  Anonymous .
+                  <TimeAgo datetime={comment.createdAt} locale="en_US" />
+                </div>
+              </div>
+            </div>
+            {comment?.imgUpload && (
+                <div className="flex gap-2 p-1 border h-20 rounded-md mt-1 ">
+                  {comment?.imgUpload.map((link, index) => (
+                    <Attachment key={index} link={link} />
+                  ))}
+                </div>
+            )}
+          </div>
+        ))}
+
+      <CommentForm feedbackId={feedbackId} />
     </div>
   );
 }
