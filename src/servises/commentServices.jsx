5afb6a91e37b0eb7777ab3fef0ebd3cf39a16ee5
ebi <ebi@gmail.com>
2024-08-05 00:10:29 +0330

    editCommentFn ceated

--- src/servises/commentServices.jsx
+++ src/servises/commentServices.jsx
@@ -7,3 +7,8 @@ import http from "./httpServises";
 export async function createCommentFn(data) {
     return http.post("/api/comment",data).then(res => res.data)
 }
+
+
+export async function editCommentFn(data) {
+    return http.put("/api/comment",data).then(res => res.data)
+}
