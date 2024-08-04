f45601ff207f990cdfdccb4676693cdde7214191
ebi <ebi@gmail.com>
2024-08-05 00:16:38 +0330

    onNewFile & smallBtn added

--- src/components/module/AttachFiles.js
+++ src/components/module/AttachFiles.js
@@ -4,7 +4,7 @@ import supabase from "@/config/SupabaseClient";
 
 
 
-function AttachFiles({setImgUpload}) {
+function AttachFiles({onNewFile,smallBtn}) {
 
   const [uploading,setUploading] = useState(false);
 
@@ -16,14 +16,16 @@ function AttachFiles({setImgUpload}) {
     const { data } = await supabase.storage.from("pictures").upload(`${file.name}`, file)
     const imageUrl = `https://dddfealtiyhdqcdfvznh.supabase.co/storage/v1/object/public/pictures/${file.name}`
     links.push(imageUrl)
-    setImgUpload((prev) => [...prev,...links])
+    // setImgUpload((prev) => [...prev,...links])
+    onNewFile(links)
     setUploading(false)
   }
 
   return (
     <label>
-          <span className={`
+          <span  className={`
           ${uploading ? "bg-gray-200 text-gray-400 cursor-not-allowed" : "bg-gray-300 hover:bg-gray-400 text-black cursor-pointer"} 
+          ${smallBtn && "text-[10px] py-[2px]"} 
             px-2 py-2 rounded-lg flex justify-center gap-2  items-center transition-colors duration-300`}>
             {uploading && <Loader width={20} height={20} color="rgb(156 163 175)"/>}
             {uploading ? "Uploading" : "Atach files"}
