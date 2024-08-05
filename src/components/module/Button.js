5a868c1099f023ee54ebe9ca673d682dd196cc77
ebi <ebi@gmail.com>
2024-08-05 13:18:14 +0330

    mediumBtn changed

--- src/components/module/Button.js
+++ src/components/module/Button.js
@@ -6,7 +6,7 @@ function Button(props){
     className={`px-2 py-2 rounded-lg flex justify-center text-black gap-1 items-center transition-colors duration-300 
      ${props.gray ? "bg-gray-300 hover:bg-gray-400":"bg-purple-300 hover:bg-purple-400"}
      ${props.smallBtn && "text-[10px] py-[2px]"}
-     ${props.mediumBtn && "text-[14px] py-1"}
+     ${props.mediumBtn && "text-[14px] py-[2px]"}
      ${props.disabled ? "bg-opacity-30 text-opacity-50 cursor-not-allowed pointer-events-none" : ""}`}>
       {props.children}
     </button>
